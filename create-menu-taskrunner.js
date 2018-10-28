ff06bb5e333b99d89c8e70ba79797073d0ffbc6c
Eric Constantinides <eric@ericconstantinides.com>
2018-10-28 01:42:43 -0700

    Cruft cleanup

--- create-menu-taskrunner.js
+++ create-menu-taskrunner.js
@@ -6,21 +6,11 @@ const menuJSON = require('./example/example-menu.json')
 // const menuWrapper = require('./example/example-index.html')
 
 let blankSiteHeader
-fs.readFile('./example/example-index.html', function(err, data) {
-  // fs.readFile('./static/site-header.html', function(err, data) {
+fs.readFile('./example/blank-index.html', function(err, data) {
   if (err) throw err
   blankSiteHeader = data.toString()
   const menu = createMenu.create(menuJSON, blankSiteHeader, '<!-- MENU_STUB -->')
-  console.log(menu)
+  fs.writeFile('./example/index.html', menu, function(err) {
+    if (err) throw err
+  })
 })
-// console.log(blankSiteHeader)
-
-// const options = {
-//   wrapper: './example/example-index.html',
-//   json: './example/example-menu.json',
-//   stub: '<!-- MENU_STUB -->',
-//   saveToPath: './',
-//   saveToFile: 'yay-go-eric-index.html'
-// }
-
-// createMenu.write(options)
